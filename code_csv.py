83d719e225a3ac12c2c61253f068acbaa092ff99
SyuanBo <godspeed88168@gmail.com>
2021-08-31 20:37:09 +0800

    PIF

--- code_csv.py
+++ code_csv.py
@@ -2,9 +2,26 @@ import json
 import os
 import pandas as pd
 
-counties = ['KHH', 'TNN', 'TXG', 'PIF']
-for i in counties:
-    os.chdir('./'+i)
-    with open(i+'.json', 'r', encoding='utf-8') as file:
+code = []
+name = []
+county = ['KHH', 'TNN', 'TXG', 'PIF']
+
+for j in county:
+    os.chdir('C:/Users/syuanbo/Desktop/landuse')
+    os.chdir(j)
+    with open(j + '.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
-    code = pd.DataFrame(list(data.items()), columns=['計畫區代碼', '計畫區名稱'])
+
+    for i in data:
+        code.append(i['計畫區代碼'])
+        name.append(i['計畫區名稱'])
+
+
+# print(code)
+# print(name)
+os.chdir('C:/Users/syuanbo/Desktop/landuse')
+df = pd.DataFrame()
+df['計畫區代碼'] = code
+df['計畫區名稱'] = name
+print(df)
+df.to_csv('code_to_name.csv', encoding='utf-8')
